2ced6f2eba1ecbdfd0280d335296c53b6104b7b0
selinahsu <selinaraindrop@gmail.com>
2020-04-25 15:02:56 -0400

    fixed package-lock in client folder

--- server.js
+++ server.js
@@ -4,17 +4,39 @@ const express = require('express');
 const app = express(); 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.get('/api/testdata', (req, res) => {
-    const testData = [
-        {id: 1, fruit: 'apple'},
-        {id: 2, fruit: 'peach'},
-        {id: 3, fruit: 'kiwi'},
-    ];
-    res.json(testData);
+  const testData = [
+      {id: 1, fruit: 'apple'},
+      {id: 2, fruit: 'peach'},
+      {id: 3, fruit: 'kiwi'},
+  ];
+  res.json(testData);
 }); 
+
+const monsters = [{ type: 'werewolf' }, { type: 'hydra' }, { type: 'chupacabra' }];
+
+/* Middleware */
+const getFlight = (req, res, next) => {
+  console.log("helooo from the middleware");
+  res.send("this is the senddd");
+  next();
+}
+
+app.get('/api/test', getFlight)
+
+//app.use(getFlight);
+
+// app.get('/api/test', (req, res) => {
+//   // axios.get(`http://www.distance24.org/route.json?stops=Hamburg|Berlin`)
+//   //   .then((res) => {
+//   //     console.log("hellooo");
+//   //   })
+//   console.log(req.params);
+//   res.send(monsters);
+// });
 
 /* Do not use 3000, that's the default for the default react-app */
 const port = process.env.port || 5000; 
