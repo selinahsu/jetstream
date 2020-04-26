e10c01fdc3417af64c9ded9a75ab75c24878cb4e
selinahsu <selinaraindrop@gmail.com>
2020-04-26 00:29:22 -0400

    revert FormCard to class component with context

--- client/src/components/form-card.js
+++ client/src/components/form-card.js
@@ -70,53 +70,60 @@ function FormContent(props) {
   );
 }
 
-function FormCard(props) {
-  const info = useContext(DataContext);
-  return (
-    <Card className="mt-5 p-5">
-      <form>
-      <Row className="justify-content-between px-3 pb-3">
-        <div>
-          <input 
-            type="radio" 
-            checked={info.flightType==="one-way"} 
-            onChange={(event) => {
-              info.setFlightType(event.target.value)
-            }}
-            id="one-way" 
-            name="flight-type" 
-            value="one-way"/>
-          <label>One Way</label>
-        </div>
-        <div>
-          <input 
-            type="radio" 
-            checked={info.flightType==="round-trip"} 
-            onChange={(event) => {
-              info.setFlightType(event.target.value)
-            }}
-            id="round-trip"
-            name="flight-type" 
-            value="round-trip"/>
-          <label>Round Trip</label>
-        </div>
-        <div>
-          <input 
-            type="radio" 
-            checked={info.flightType==="multi-city"} 
-            onChange={(event) => {
-              info.setFlightType(event.target.value)
-            }}
-            id="multi-city" name="flight-type" 
-            value="multi-city"/>
-          <label>Multi-City</label>
-        </div>
-      </Row>
-      </form>
-      <UnavailableMssg/>
-      <FormContent/>
-    </Card>
-  );
+class FormCard extends React.Component {
+  static contextType = DataContext;
+  state = {
+
+  }
+  render() {
+    /* Destructure the context */
+    const { flightType, destination, departure, seat, setFlightType } = this.context;
+    return (
+      <Card className="mt-5 p-5">
+        <form>
+        <Row className="justify-content-between px-3 pb-3">
+          <div>
+            <input 
+              type="radio" 
+              checked={flightType==="one-way"} 
+              onChange={(event) => {
+                setFlightType(event.target.value)
+              }}
+              id="one-way" 
+              name="flight-type" 
+              value="one-way"/>
+            <label>One Way</label>
+          </div>
+          <div>
+            <input 
+              type="radio" 
+              checked={flightType==="round-trip"} 
+              onChange={(event) => {
+                setFlightType(event.target.value)
+              }}
+              id="round-trip"
+              name="flight-type" 
+              value="round-trip"/>
+            <label>Round Trip</label>
+          </div>
+          <div>
+            <input 
+              type="radio" 
+              checked={flightType==="multi-city"} 
+              onChange={(event) => {
+                setFlightType(event.target.value)
+              }}
+              id="multi-city" name="flight-type" 
+              value="multi-city"/>
+            <label>Multi-City</label>
+          </div>
+        </Row>
+        </form>
+        <UnavailableMssg/>
+        <FormContent/>
+      </Card>
+    );
+  }
 }
 
 export default FormCard; 
